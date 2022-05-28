b3da0a9c0e2e8f4eaec29eafac5561b7c2cf2d6d
curiousKidd <skasrjhaan@gmail.com>
2022-05-28 23:49:16 +0900

    20220528(토) - HTTP Request Data _ get

--- src/main/java/hello/servlet/basic/request/RequestParamServlet.java
+++ src/main/java/hello/servlet/basic/request/RequestParamServlet.java
@@ -10,13 +10,38 @@ import java.util.Enumeration;
 
 /**
  * 1. 파라미터 전송 기능
- * http:// localhost:8080/request-param?username=hello&age=20
+ * http://localhost:8080/request-param?username=hello&age=20
  */
 @WebServlet(name = "requestParamServlet", urlPatterns = "/request-param")
 public class RequestParamServlet extends HttpServlet {
 
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        Enumeration<String> parameterNames = request.getParameterNames();
+        System.out.println("[전체 파라미터 조회] - start");
+
+        //getParameterNames == key
+        //getParameter == value
+        request.getParameterNames().asIterator()
+                .forEachRemaining(paramName -> System.out.println(paramName + "=" + request.getParameter(paramName)));
+
+        System.out.println("[전체 파라미터 조회] - end");
+
+
+        System.out.println("[단일 파라미터 조회]");
+        String username = request.getParameter("username");
+        String age = request.getParameter("age");
+
+        System.out.println("username = " + username);
+        System.out.println("age = " + age);
+
+        System.out.println("[이름이 같은 복수 파라미터 조회]");
+        // 중복일 때 getParameterValues() 사용
+        String[] usernames = request.getParameterValues("username");
+        for (String name : usernames) {
+            System.out.println("usernames = " + name);
+        }
+
+        response.getWriter().write("ok");
+
     }
 }
