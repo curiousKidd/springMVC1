5ca60f2ad35f3f2baef305c28de7d722f4897c31
curiousKidd <skasrjhaan@gmail.com>
2022-07-06 23:55:00 +0900

    20220706(수) - 컴포넌트 스캔을 안쓰고 스프링 빈 등록

--- src/main/java/hello/servlet/web/springmvc/v1/SpringMemberFormControllerV1.java
+++ src/main/java/hello/servlet/web/springmvc/v1/SpringMemberFormControllerV1.java
@@ -4,12 +4,21 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+//컴포넌트 스캔을 통해 스프링 빈으로 등록
+// @Component
+// @RequestMapping
+
 @Controller
 public class SpringMemberFormControllerV1 {
 
+    // @Component 컴포넌트 스캔없이 스프링 빈으로 직접 등록해도 정상 동작
+    // ServletApplication
     @RequestMapping("/springmvc/v1/members/new-form")
     public ModelAndView process() {
         return new ModelAndView("new-form");
     }
 
 }
+
+
+
