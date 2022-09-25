5fdb1dd0cdf3c2d1d26c483018e5a348ed24bf55
curiousKidd <skasrjhaan@gmail.com>
2022-09-25 23:31:42 +0900

    20220925(일) - tomcat 구동오류 수정

--- src/main/java/hello/servlet/ServletApplication.java
+++ src/main/java/hello/servlet/ServletApplication.java
@@ -1,10 +1,8 @@
 package hello.servlet;
 
-import hello.servlet.web.springmvc.v1.SpringMemberFormControllerV1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.context.annotation.Bean;
 
 @ServletComponentScan
 @SpringBootApplication
@@ -15,9 +13,9 @@ public class ServletApplication {
     }
 
     //스프링 빈 직접 등록
-    @Bean
-    SpringMemberFormControllerV1 springMemberFormControllerV1() {
-        return new SpringMemberFormControllerV1();
-    }
+    //    @Bean
+    //    SpringMemberFormControllerV1 springMemberFormControllerV1() {
+    //        return new SpringMemberFormControllerV1();
+    //    }
 
 }
